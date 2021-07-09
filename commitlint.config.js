f7d84e59ae55cd0de06947a4adae3a9b24350319
Diana Lau <dhmlau@ca.ibm.com>
2021-07-08 21:02:45 -0400

    ci: add GitHub Actions
    Signed-off-by: Diana Lau <dhmlau@ca.ibm.com>

---
+++ commitlint.config.js
@@ -0,0 +1,16 @@
+// Copyright IBM Corp. 2021. All Rights Reserved.
+// Node module: loopback-connector-mysql
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+module.exports = {
+    extends: [
+      '@commitlint/config-conventional',
+    ],
+    rules: {
+      'header-max-length': [2, 'always', 100],
+      'body-leading-blank': [2, 'always'],
+      'footer-leading-blank': [0, 'always'],
+      'signed-off-by': [2, 'always', 'Signed-off-by:'],
+    },
+  };
